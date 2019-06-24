110e423b97775b387a5992b6c9abac1cfb5f5dd2
web525 <192389590@qq.com>
2019-06-24 11:49:18 +0800

    重新调整结构

--- src/middleware/api/index/index.js
+++ src/middleware/api/index/index.js
@@ -32,129 +32,126 @@ class Api {
     async FB_GetInfo_chi() {// 2.2.4.7
         const resData = await this.getData('/xml/index/FB_GetInfo_chi.xml');
         const { Coupons: { CouponInfo }, TournamentPools } = resData.AOSBS_XML;
-        let result = { dat: [] };
+        let result = { data: [] };
         try {
             CouponInfo.map((item1, index1) => {
                 // Jim@2019-06-17 17:43:23：fix xml2json bug
                 let { Matches: { MatchInfo } } = item1;
                 MatchInfo = obj2Arr(MatchInfo);
-                // get title
-                let d_item = {};
-                d_item.title = `${MatchInfo[0] ? MatchInfo[0].Date : ''}(${item1.NameShort})`;
-                d_item.data = [];            
+                
+                let dataPerDay = {};// 每天赛事
+                // 赛事日期
+                dataPerDay.date = `${MatchInfo[0] ? MatchInfo[0].Date : ''}(${item1.NameShort})`;
+                dataPerDay.coupons = [];// 赛事列表          
                 // console.log(index1,item1)
                 MatchInfo.map((item2, index2) => {
                     // console.log(index1+'_'+index2, item2.Pools.PoolInfo)
+                    let coupons_item = {};// 每场赛事
+                    coupons_item.matches = [];// 比赛列表
+
                     const getPoolInfo = (PoolInfo, poolType='')=>{
                         PoolInfo.map((item3, index3) => {
                             if(item3.Pool == 'HFMP'){// 6宝半全场
                                 getPoolInfo(item3.LegInfo, 'HFMP');
                             }else{// 普通类型
-                                let d_d_item = {};
+                                let matches_item ={};// 每场比赛
 
-                                // 处理玩法（二级）
-                                d_d_item.div = item2.League;
-                                d_d_item.team1 = item2.Home;
-                                d_d_item.team2 = item2.Away;
-                                d_d_item.score1 = item2.Progress.Result.split(':')[0];
-                                d_d_item.score2 = item2.Progress.Result.split(':')[1];
-                                d_d_item.timing = item2.MatchPoolCloseTime + ' ' + item2.MatchPoolCloseDate;
-                                // 未知字段
-                                d_d_item.rate1_title = '';
-                                d_d_item.rate2_title = '';
-                                d_d_item.rate3_title = '';
-                                d_d_item.rate1_num = '';
-                                d_d_item.rate2_num = '';
-                                d_d_item.rate3_num = '';
-
-                                // 处理赔率（三级）
                                 let pool = poolType||item3.Pool;
+                                matches_item.league = item2.League;// 国家地区
+                                matches_item.home = item2.Home;
+                                matches_item.away = item2.Away;
+                                matches_item.score_Home = item2.Progress.Result.split(':')[0];
+                                matches_item.score_Away = item2.Progress.Result.split(':')[1];
+                                matches_item.closeTime = item2.MatchPoolCloseTime + ' ' + item2.MatchPoolCloseDate;                                
+
+                                matches_item.pool = pool;// 投注类型
+                                matches_item.poolNum = PoolInfo.length;
+                                matches_item.corner = item2.Progress.Corner;
+                                matches_item.inplay = item2.Inplay;
+                                matches_item.OddsSet  = [];// 每场比赛的投注池
+
                                 item3.OddsSet = obj2Arr(item3.OddsSet);
                                 item3.OddsSet.map((item4,index4)=>{
-                                    // 每个pool单独处理
-                                    if(pool=='HAD'){// 主客和
-                                        item4.OddsInfo.map((item6,index6)=>{
-                                            if(item6.Number==1){// Home
-                                                d_d_item[pool + '_title1'] = 'H';
-                                                d_d_item[pool + '_rate1'] = item6.Odds;
-                                            }else if(item6.Number=='X'){// D
-                                                d_d_item[pool + '_title2'] = 'D';
-                                                d_d_item[pool + '_rate2'] = item6.Odds;
-                                            }else if(item6.Number==2){// Away
-                                                d_d_item[pool + '_title3'] = 'A';
-                                                d_d_item[pool + '_rate3'] = item6.Odds;
-                                            }
-                                        })
-                                        // console.log(pool,index1+'_'+index2+'_'+index3+'_'+index4,d_d_item)
-                                    }else if(pool=='FHAD'){// 预测一场球赛的上半场赛果
-                                        // no data
-                                    }else if(pool=='HHAD'){// 较强之球队让球于较弱之球队
-                                        // no data
-                                    }else if(pool=='HDC'){// 投注经让球调整后之赛果
-                                        item4.OddsInfo.map((item6,index6)=>{
+                                    let pools_item = {};// 每个投注项
+                                    pools_item.enabled = item3.Enabled;
+                                    pools_item.stopSell = item3.StopSell;
+                                    pools_item.oddsInfo = [];// 每个投注项的赔率列表
+
+                                    item4.OddsInfo = obj2Arr(item4.OddsInfo);
+                                    item4.OddsInfo.map((item6,index6)=>{
+                                        let oddsInfo_item = {};// 每种赔率详情
+                                        if(pool=='HAD'){// 主客和
+                                                if(item6.Number==1){// Home
+                                                    oddsInfo_item.name = 'H';
+                                                }else if(item6.Number=='X'){// D
+                                                    oddsInfo_item.name = 'D';
+                                                }else if(item6.Number==2){// Away
+                                                    oddsInfo_item.name = 'A';
+                                                }
+                                                oddsInfo_item.odds = item6.Odds;
+                                            // console.log(pool,index1+'_'+index2+'_'+index3+'_'+index4,matches_item)
+                                        }else if(pool=='FHAD'){// 预测一场球赛的上半场赛果
+                                            // no data
+                                        }else if(pool=='HHAD'){// 较强之球队让球于较弱之球队
+                                            // no data
+                                        }else if(pool=='HDC'){// 投注经让球调整后之赛果
                                             if(item6.Number==1){// 
-                                                d_d_item[pool + '_title1'] = 'H';
-                                                d_d_item[pool + '_rate1'] = item6.Odds;
+                                                matches_item[pool + '_title1'] = 'H';
+                                                matches_item[pool + '_rate1'] = item6.Odds;
                                             }else if(item6.Number==2){// 
-                                                d_d_item[pool + '_title3'] = 'C';
-                                                d_d_item[pool + '_rate3'] = item6.Odds;
+                                                matches_item[pool + '_title3'] = 'C';
+                                                matches_item[pool + '_rate3'] = item6.Odds;
                                             }
-                                        })
-                                        d_d_item[pool + '_title2'] = 'D';
-                                        d_d_item[pool + '_rate2'] = 0;
-                                    }else if(pool=='HFT'){// 预测球赛中半场(45分钟)及全场(90分钟)之主客和赛果
+                                        }else if(pool=='HFT'){// 预测球赛中半场(45分钟)及全场(90分钟)之主客和赛果
 
-                                    }else if(pool=='TQL'){// 在指定赛事，投注哪队能晋级下一场赛事
+                                        }else if(pool=='TQL'){// 在指定赛事，投注哪队能晋级下一场赛事
 
-                                    }else if(pool=='CRS'){// 预测球赛的正式比分
+                                        }else if(pool=='CRS'){// 预测球赛的正式比分
 
-                                    }else if(pool=='FCS'){// 投注一场球赛法定时间上半场45分钟及上半场补时的比分
+                                        }else if(pool=='FCS'){// 投注一场球赛法定时间上半场45分钟及上半场补时的比分
 
-                                    }else if(pool=='TTG'){// 预测球赛中两队的入球个数
+                                        }else if(pool=='TTG'){// 预测球赛中两队的入球个数
 
-                                    }else if(pool=='OOE'){// 预测球赛中两队的入球个数为单数或双数
+                                        }else if(pool=='OOE'){// 预测球赛中两队的入球个数为单数或双数
 
-                                    }else if(pool=='HILO'){// 投注者可就一场球赛的入球个数高于或低于预判者所指定的数目(中位球数)下注
+                                        }else if(pool=='HILO'){// 投注者可就一场球赛的入球个数高于或低于预判者所指定的数目(中位球数)下注
 
-                                    }else if(pool=='FHL'){// 投注一场球赛的上半场入球个数，多于或少于指定的球数
+                                        }else if(pool=='FHL'){// 投注一场球赛的上半场入球个数，多于或少于指定的球数
 
-                                    }else if(pool=='CHLO'){// 投注于指定球赛中，两队合共开出的角球个数*多于或少于指定的角球数
+                                        }else if(pool=='CHLO'){// 投注于指定球赛中，两队合共开出的角球个数*多于或少于指定的角球数
 
-                                    }else if(pool=='PS'){// 不包括加时或互射12码后的赛果
+                                        }else if(pool=='PS'){// 不包括加时或互射12码后的赛果
 
-                                    }else if(pool=='GPW'){// 投注分组阶段小组首名出线队伍
+                                        }else if(pool=='GPW'){// 投注分组阶段小组首名出线队伍
 
-                                    }else if(pool=='GPF'){// 投注分组阶段小组的顺序首、次出线队伍
+                                        }else if(pool=='GPF'){// 投注分组阶段小组的顺序首、次出线队伍
 
-                                    }else if(pool=='TPS'){// 投注整个锦标赛射入最多入球的球员
+                                        }else if(pool=='TPS'){// 投注整个锦标赛射入最多入球的球员
 
-                                    }else if(pool=='FGS'){// 投注一场球赛中最先射入对方球门得分的球员
+                                        }else if(pool=='FGS'){// 投注一场球赛中最先射入对方球门得分的球员
 
-                                    }else if(pool=='FTS'){// 投注哪队于法定时间(90分钟)获得第一个入球或无入球
+                                        }else if(pool=='FTS'){// 投注哪队于法定时间(90分钟)获得第一个入球或无入球
 
-                                    }else if(pool=='CP'){// 在指定赛事，投注哪队能赢得冠军
+                                        }else if(pool=='CP'){// 在指定赛事，投注哪队能赢得冠军
 
-                                    }else if(pool=='DHCP'){// 预测两场球赛中板厂(45分钟)及全场(90分钟)的正式比分
+                                        }else if(pool=='DHCP'){// 预测两场球赛中板厂(45分钟)及全场(90分钟)的正式比分
 
-                                    }else if(pool=='6FH'){// 6宝半全场
+                                        }else if(pool=='6FH'){// 6宝半全场
 
-                                    }                                
+                                        }
+                                        pools_item.oddsInfo.push(oddsInfo_item);
+                                    })   
+                                    matches_item.OddsSet.push(pools_item);                       
                                 })
-                                // console.log(index1+'_'+index2+'_'+index3,pool,d_d_item)
 
-                                d_d_item.poolType = pool;
-                                d_d_item.pool = PoolInfo.length;
-                                d_d_item.corner = item2.Progress.Corner;
-                                d_d_item.tab_title = '';
-                                d_d_item.tab_num = '';
-
-                                d_item.data.push(d_d_item);
+                                coupons_item.matches.push(matches_item);
                             }
                         })                        
                     }
                     getPoolInfo(item2.Pools.PoolInfo);
+                    dataPerDay.coupons.push(coupons_item);
                 })
-                result.dat.push(d_item);
+                result.data.push(dataPerDay);
             })
 
             this.tmp.CouponInfo = CouponInfo;
--- src/middleware/doc/Football/FB_GetInfo_chi.js
+++ src/middleware/doc/Football/FB_GetInfo_chi.js
@@ -14,52 +14,53 @@
  * 
  * @apiSuccess (Reponse 200) {Object[]} data
  * @apiSuccess (Reponse 200) {String} data.date 比赛日期 (title)
- * @apiSuccess (Reponse 200) {Object[]} data.Coupons 比赛列表
- * @apiSuccess (Reponse 200) {String} data.Coupons.League 国家 (div)
- * @apiSuccess (Reponse 200) {String} data.Coupons.pool 投注类型
- * @apiSuccess (Reponse 200) {Object[]} data.Coupons.Matches 投注类型
- * @apiSuccess (Reponse 200) {String} data.Coupons.Matches.Home 主队 (team1)
- * @apiSuccess (Reponse 200) {String} data.Coupons.Matches.Away 客队 (team2)
- * @apiSuccess (Reponse 200) {String} data.Coupons.Matches.score_Home 主队比分 (score1)
- * @apiSuccess (Reponse 200) {String} data.Coupons.Matches.score_Away 客队比分 (score2)
- * @apiSuccess (Reponse 200) {String} data.Coupons.Matches.CloseTime 结束时间 (timing)
- * @apiSuccess (Reponse 200) {String} data.Coupons.Matches.poolNum 投注类型总数 (pool)
- * @apiSuccess (Reponse 200) {String} data.Coupons.Matches.corner 角球 (corner)
- * @apiSuccess (Reponse 200) {String} data.Coupons.Matches.Inplay Inplay
- * @apiSuccess (Reponse 200) {Object[]} data.Coupons.Matches.Pools 投注池
- * @apiSuccess (Reponse 200) {Object[]} data.Coupons.Matches.Pools.Enabled
- * @apiSuccess (Reponse 200) {Object[]} data.Coupons.Matches.Pools.StopSell 停售时间
- * @apiSuccess (Reponse 200) {Object[]} data.Coupons.Matches.Pools.OddsSets 赔率列表
- * @apiSuccess (Reponse 200) {String} data.Coupons.Matches.Pools.OddsSets.name 赔率标识
- * @apiSuccess (Reponse 200) {String} data.Coupons.Matches.Pools.OddsSets.Odds 赔率值
+ * @apiSuccess (Reponse 200) {Object[]} data.coupons 比赛列表
+ * @apiSuccess (Reponse 200) {String} data.coupons.league 国家 (div)
+ * @apiSuccess (Reponse 200) {Object[]} data.coupons.matches 投注类型
+ * @apiSuccess (Reponse 200) {String} data.coupons.matches.home 主队 (team1)
+ * @apiSuccess (Reponse 200) {String} data.coupons.matches.away 客队 (team2)
+ * @apiSuccess (Reponse 200) {String} data.coupons.matches.score_Home 主队比分 (score1)
+ * @apiSuccess (Reponse 200) {String} data.coupons.matches.score_Away 客队比分 (score2)
+ * @apiSuccess (Reponse 200) {String} data.coupons.matches.closeTime 结束时间 (timing)
+ * @apiSuccess (Reponse 200) {String} data.coupons.matches.pool 投注类型
+ * @apiSuccess (Reponse 200) {String} data.coupons.matches.poolNum 投注类型总数 (pool)
+ * @apiSuccess (Reponse 200) {String} data.coupons.matches.corner 角球 (corner)
+ * @apiSuccess (Reponse 200) {String} data.coupons.matches.inplay Inplay
+ * @apiSuccess (Reponse 200) {Object[]} data.coupons.matches.pools 投注池
+ * @apiSuccess (Reponse 200) {String} data.coupons.matches.pools.enabled
+ * @apiSuccess (Reponse 200) {String} data.coupons.matches.pools.stopSell 停售时间
+ * @apiSuccess (Reponse 200) {Object[]} data.coupons.matches.pools.oddsSets 赔率列表
+ * @apiSuccess (Reponse 200) {String} data.coupons.matches.pools.oddsSets.name 赔率标识
+ * @apiSuccess (Reponse 200) {String} data.coupons.matches.pools.oddsSets.odds 赔率值
  * 
- * @apiSuccessExample {json} Response:
+ * @apiSuccessExample {json} Success-Response: 
+ * HTTP/1.1 200 OK
  * {
  *     data: [
  *          {
- *              date: '08/10 (THU)',// 比赛日期 (title)
- *              Coupons: [// 比赛列表
+ *              date: '08/10 (THU)',    // 比赛日期 (title)
+ *              coupons: [              // 比赛列表
  *                  {
- *                      League: '歐洲國家盃',// 国家 (div)
- *                      pool: 'HDA',// 投注类型 (新增)
- *                      Matches:[
+ *                      league: '歐洲國家盃',        // 国家 (div)
+ *                      matches:[
  *                          {
- *                              Home: '中国',// 主队 (team1)
- *                              Away: '巴西',// 客队 (team2)
- *                              score_Home: '1',// 主队比分 (score1)
- *                              score_Away: '2',// 客队比分 (score2)
- *                              CloseTime: '19:00,21/06',// 结束时间 (timing)
- *                              poolNum: '3',// 投注类型总数 (pool)
- *                              corner: '0',// 角球 (corner)
- *                              Inplay: '0',
- *                              Pools : [// 投注池
+ *                              home: '中国',               // 主队 (team1)
+ *                              away: '巴西',               // 客队 (team2)
+ *                              score_Home: '1',            // 主队比分 (score1)
+ *                              score_Away: '2',            // 客队比分 (score2)
+ *                              closeTime: '19:00,21/06',   // 结束时间 (timing)
+ *                              pool: 'HDA',                // 投注类型 (新增)
+ *                              poolNum: '3',               // 投注类型总数 (pool)
+ *                              corner: '0',                // 角球 (corner)
+ *                              inplay: '0',
+ *                              pools : [            // 投注池
  *                                  {
- *                                      Enabled: '0',
- *                                      StopSell: '08/10 10:30',// 停售时间
- *                                      OddsSets: [// 赔率列表
+ *                                      enabled: '0',
+ *                                      stopSell: '08/10 10:30', // 停售时间
+ *                                      oddsSets: [              // 赔率列表
  *                                          {
- *                                              name: 'H',// 赔率标识
- *                                              Odds: '1.2',// 赔率值
+ *                                              name: 'H',              // 赔率标识
+ *                                              odds: '1.2',            // 赔率值
  *                                          }
  *                                      ]
  *                                  }
