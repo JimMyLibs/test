7a0eb5edd9ab939900abea9e7e0830de26b07e72
web525 <192389590@qq.com>
2019-07-22 11:42:55 +0800

    test case OK

--- src/middleware/test/index/getFilterList.test.js
+++ src/middleware/test/index/getFilterList.test.js
@@ -9,7 +9,7 @@ beforeAll(async () => {
 });
 
 
-describe('begin to test filterList', async () => {
+describe('begin to test filterList', () => {
     test('test poolsList', () => {
         const { poolList, leagueList, dateList } = filterList;
         const expectData = {
@@ -19,10 +19,18 @@ describe('begin to test filterList', async () => {
     });
     test('test dateList', () => {
         const { poolList, leagueList, dateList } = filterList;
-        const reg_date = /^\d{1,2}\/\d{1,2}\([A-Z]{3}\)$/;
+        const reg_date = /^\d{1,2}\/\d{1,2}\([A-Z]{3}\)$/;// 08/10(THU)
         dateList.map((item,index)=>{
             // if(index==3){ item = item+'-' }// make a wrong item
             expect(item).toMatch(reg_date)
+        })
+    });
+    test('test leagueList', () => {
+        const { poolList, leagueList, dateList } = filterList;
+        const reg_league = /^[\u4e00-\u9fa5]{1,}$|^[a-zA-Z]+$/;// 歐洲國家盃 || abc
+        leagueList.map((item,index)=>{
+            // if(index==3){ item = item+'-' }// make a wrong item
+            expect(item).toMatch(reg_league)
         })
     });
 })
