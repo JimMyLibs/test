d74ac1099158ed09f1886ef68f000b5440b59ec4
web525 <192389590@qq.com>
2019-08-08 10:47:19 +0800

    update tslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,11 +33,11 @@ module.exports = {
         // "off" 或 0 - 关闭规则
         // "warn" 或 1 - 开启规则，使用警告级别的错误：warn (不会导致程序退出),
         // "error" 或 2 - 开启规则，使用错误级别的错误：error (当被触发的时候，程序会退出)
-        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',// 禁用 console
-        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',// 禁用 debugger
-        "no-unused-vars": 2,// 禁止出现未使用过的变量
+        'no-console': process.env.NODE_ENV === 'production' ? 2 : 0,// 禁用 console
+        'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,// 禁用 debugger
+        "no-unused-vars": 1,// 禁止出现未使用过的变量
         "linebreak-style": 0,// 强制使用一致的换行风格
-        "quotes": [2, "single", { "allowTemplateLiterals": true }],
+        "quotes": [1, "single", { "allowTemplateLiterals": true }],
         // used to ts
         "import-name": 0,// the name from import must be declared
         "ban-types": 0,// 
